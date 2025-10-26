fe40d04336f9800d6d1673d2931457b2c87fd580
MenaceHecker <thedynamic2508@gmail.com>
2025-10-26 12:00:13 -0400

    Created home view

---
+++ movieIt/HomeView.swift
@@ -0,0 +1,18 @@
+//
+//  HomeView.swift
+//  movieIt
+//
+//  Created by Tushar Mishra on 10/26/25.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    HomeView()
+}
