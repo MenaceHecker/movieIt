a03e4f180d14e8d5ddff9b95c63a21a195fc1101
MenaceHecker <thedynamic2508@gmail.com>
2025-10-26 10:56:14 -0400

    Initial Commit

---
+++ movieIt/movieItApp.swift
@@ -0,0 +1,17 @@
+//
+//  movieItApp.swift
+//  movieIt
+//
+//  Created by Tushar Mishra on 10/26/25.
+//
+
+import SwiftUI
+
+@main
+struct movieItApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
