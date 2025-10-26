fa77f4f951933f887902b299dbaa76612a317940
MenaceHecker <thedynamic2508@gmail.com>
2025-10-26 11:49:31 -0400

    Added constants

---
+++ movieIt/Constants.swift
@@ -0,0 +1,14 @@
+//
+//  Constants.swift
+//  movieIt
+//
+//  Created by Tushar Mishra on 10/26/25.
+//
+
+import Foundation
+struct Constants{
+    static let homeString = "Home"
+    static let searchString = "Search"
+    static let upcomingString = "Upcoming"
+    static let downloadString = "Download"
+}
