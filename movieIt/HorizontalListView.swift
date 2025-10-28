42e3c50ad9c465c451d2dcb2bcee24ef7f65ee04
MenaceHecker <thedynamic2508@gmail.com>
2025-10-28 10:23:41 -0400

    Created Horizontal View

---
+++ movieIt/HorizontalListView.swift
@@ -0,0 +1,18 @@
+//
+//  HorizontalListView.swift
+//  movieIt
+//
+//  Created by Tushar Mishra on 10/28/25.
+//
+
+import SwiftUI
+
+struct HorizontalListView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    HorizontalListView()
+}
