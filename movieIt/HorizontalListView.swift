ac2c770e28bb5566ce8e364a96aabf2c6b29fcff
MenaceHecker <thedynamic2508@gmail.com>
2025-10-28 10:25:08 -0400

    Added header to horizontal view

--- movieIt/HorizontalListView.swift
+++ movieIt/HorizontalListView.swift
@@ -8,6 +8,10 @@
 import SwiftUI
 
 struct HorizontalListView: View {
+    let header = Constants.trendingMovieString
+    var titles = [Constants.testTitleURL, Constants.testTitleURL2, Constants.testTitleURL3]
+    
+    
     var body: some View {
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
     }
