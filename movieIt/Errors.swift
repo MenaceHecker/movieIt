3c003532f72af39ee89eb55cae6c1972b72f971a
MenaceHecker <thedynamic2508@gmail.com>
2025-10-30 19:26:04 -0400

    Added Error Description

--- movieIt/Errors.swift
+++ movieIt/Errors.swift
@@ -10,4 +10,15 @@ enum APIConfigError: Error, LocalizedError{
     case fileNotFound
     case dataLoadingFailed(underlyingError: Error)
     case decodingFailed(underlyingError: Error)
+    
+    var errorDescription: String?{
+        switch self {
+        case .fileNotFound:
+            return "File not found"
+        case .dataLoadingFailed(underlyingError: let error):
+            return "Failed to load data with error: \(error)"
+        case .decodingFailed(underlyingError: let error):
+            return "Failed to decode data with error: \(error)"
+        }
+    }
 }
