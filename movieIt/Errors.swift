0674c8a3fc14b70959cef95373a805976dcaafe0
MenaceHecker <thedynamic2508@gmail.com>
2025-10-30 19:24:46 -0400

    Added error cases

--- movieIt/Errors.swift
+++ movieIt/Errors.swift
@@ -6,3 +6,8 @@
 //
 
 import Foundation
+enum APIConfigError: Error, LocalizedError{
+    case fileNotFound
+    case dataLoadingFailed(underlyingError: Error)
+    case decodingFailed(underlyingError: Error)
+}
