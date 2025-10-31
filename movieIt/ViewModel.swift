44e2d087e6c81569564f1568a04590f228a43fa1
MenaceHecker <thedynamic2508@gmail.com>
2025-10-31 13:28:15 -0400

    View Model almost done

--- movieIt/ViewModel.swift
+++ movieIt/ViewModel.swift
@@ -16,4 +16,17 @@ class ViewModel{
         case failed(underlyingError: Error)
     }
     private(set) var homeStatus: FetchStatus = .notStarted
+    private let dataFetcher = DataFetcher()
+    var trendingMovies : [Title] = []
+    
+    func getTitles() async{
+        homeStatus = .fetching
+        do{
+            trendingMovies = try await dataFetcher.fetchTitles(for: "movie")
+            homeStatus = .success
+        } catch {
+            print(error)
+            homeStatus = .failed(underlyingError: error)
+        }
+    }
 }
