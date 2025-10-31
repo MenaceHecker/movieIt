4cef86c47a6b62db00580b24b683df10cbdc840f
MenaceHecker <thedynamic2508@gmail.com>
2025-10-30 20:26:51 -0400

    Added decoder

--- movieIt/Errors.swift
+++ movieIt/Errors.swift
@@ -26,12 +26,16 @@ enum APIConfigError: Error, LocalizedError{
 enum NetworkError: Error, LocalizedError{
     case badURLResponse(underlyingError: Error)
     case missingConfig
+    case urlBuildFailed
     var errorDescription: String?{
         switch self {
             case .badURLResponse(underlyingError: let error):
             return "Bad URL Response with error: \(error)"
         case .missingConfig:
             return "Missing Configuration"
+        
+        case .urlBuildFailed:
+            return "Failed to build URL"
         }
     }
 }
