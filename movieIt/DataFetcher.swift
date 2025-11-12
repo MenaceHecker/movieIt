a3ab1c27bb696645a2c7763b01a8930e4a32a991
MenaceHecker <thedynamic2508@gmail.com>
2025-11-12 13:05:37 -0500

    get url func complete

--- movieIt/DataFetcher.swift
+++ movieIt/DataFetcher.swift
@@ -14,20 +14,7 @@ struct DataFetcher{
     
     
     func fetchTitles(for media:String, by type:String) async throws -> [Title] {
-        guard let baseURL = tmdbBaseURL else{
-            throw NetworkError.missingConfig
-        }
-        guard let apiKey = tmdbAPIKey else {
-            throw NetworkError.missingConfig
-        }
-        
-        guard let fetchTitlesURL = URL (string: baseURL)?
-            .appending(path: "3/trending/\(media)/day")
-            .appending(queryItems: [
-                URLQueryItem(name: "api_key", value: apiKey)
-            ]) else{
-            throw NetworkError.urlBuildFailed
-        }
+       
         print(fetchTitlesURL)
         let (data,urlResponse) = try await URLSession.shared.data(from: fetchTitlesURL)
         
@@ -42,5 +29,33 @@ struct DataFetcher{
         var titles = try decoder.decode(APIObject.self, from: data).results
         Constants.addPosterPath(to: &titles)
         return titles
+    }
+    
+    private func buildURL (media: String, type: String) throws -> URL? {
+        guard let baseURL = tmdbBaseURL else{
+            throw NetworkError.missingConfig
+        }
+        guard let apiKey = tmdbAPIKey else {
+            throw NetworkError.missingConfig
+        }
+        var path:String
+        if type == "trending" {
+            path = "3/trending/\(media)/day"
+        }
+        else if type == "top_rated" {
+            path = "3/\(media)/top_rated"
+        }
+        else {
+            throw NetworkError.urlBuildFailed
+        }
+        
+        guard let url = URL (string: baseURL)?
+            .appending(path: path)
+            .appending(queryItems: [
+                URLQueryItem(name: "api_key", value: apiKey)
+            ]) else{
+            throw NetworkError.urlBuildFailed
+        }
+        return url
     }
 }
