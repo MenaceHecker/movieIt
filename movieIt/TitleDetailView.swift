7fdb2828f8cad55d83eef35204b166b479346f31
MenaceHecker <thedynamic2508@gmail.com>
2025-11-12 16:42:30 -0500

    Created Title Detail View

---
+++ movieIt/TitleDetailView.swift
@@ -0,0 +1,19 @@
+//
+//  TitleDetailView.swift
+//  movieIt
+//
+//  Created by Tushar Mishra on 11/12/25.
+//
+
+import SwiftUI
+
+struct TitleDetailView: View {
+    let title: Title
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TitleDetailView()
+}
