4e57fe3f1bec4ed0b3476da4252f6deff93c3e59
MenaceHecker <thedynamic2508@gmail.com>
2025-11-12 16:28:15 -0500

    Added populating data only when empty setup

--- movieIt/ViewModel.swift
+++ movieIt/ViewModel.swift
@@ -24,20 +24,28 @@ class ViewModel{
     
     func getTitles() async{
         homeStatus = .fetching
-        do{
-            async let tMovies = dataFetcher.fetchTitles(for: "movie", by: "trending")
-            async let tTv = dataFetcher.fetchTitles(for: "tv", by: "trending")
-            async let tRMovies = dataFetcher.fetchTitles(for: "movie", by: "top_rated")
-            async let tRTV = dataFetcher.fetchTitles(for: "tv", by: "top_rated")
+        if(trendingMovies.isEmpty)
+        {
             
-            trendingMovies = try await tMovies
-            trendingTV = try await tTv
-            topRatedMovies = try await tRMovies
-            topRatedTV = try await tRTV
-            homeStatus = .success
-        } catch {
-            print(error)
-            homeStatus = .failed(underlyingError: error)
+            do{
+                async let tMovies = dataFetcher.fetchTitles(for: "movie", by: "trending")
+                async let tTv = dataFetcher.fetchTitles(for: "tv", by: "trending")
+                async let tRMovies = dataFetcher.fetchTitles(for: "movie", by: "top_rated")
+                async let tRTV = dataFetcher.fetchTitles(for: "tv", by: "top_rated")
+                
+                trendingMovies = try await tMovies
+                trendingTV = try await tTv
+                topRatedMovies = try await tRMovies
+                topRatedTV = try await tRTV
+                homeStatus = .success
+            } catch {
+                print(error)
+                homeStatus = .failed(underlyingError: error)
+            }
         }
+        else {
+            homeStatus = .success
+        }
+        
     }
 }
