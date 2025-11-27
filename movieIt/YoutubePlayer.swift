88c6dcaf11db515db62d9cbf614b5993ec064108
MenaceHecker <thedynamic2508@gmail.com>
2025-11-27 18:39:10 -0500

    Completed Youtube Player

--- movieIt/YoutubePlayer.swift
+++ movieIt/YoutubePlayer.swift
@@ -5,4 +5,24 @@
 //  Created by Tushar Mishra on 11/27/25.
 //
 
-import Foundation
+import SwiftUI
+import WebKit
+
+struct YoutubePlayer: UIViewRepresentable{
+    let webView = WKWebView()
+    let videoID: String
+    let youtubeBaseURL = APIConfig.shared?.youtubeBaseURL
+    
+    func makeUIView(context: Context) -> some UIView {
+        webView
+    }
+    
+    func updateUIView(_ uiView: UIViewType, context: Context) {
+        guard let baseURLString = youtubeBaseURL,
+              let baseURL = URL(string: baseURLString) else {
+            return
+        }
+        let fullURL = baseURL.appending(path: videoID)
+        webView.load(URLRequest(url: fullURL))
+    }
+}
