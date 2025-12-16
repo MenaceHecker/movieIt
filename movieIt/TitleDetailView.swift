cb4c67b4a43bd4d285d9e6d150776824553d0b35
MenaceHecker <thedynamic2508@gmail.com>
2025-12-16 15:52:23 -0500

    YouTubePlayerView works

--- movieIt/TitleDetailView.swift
+++ movieIt/TitleDetailView.swift
@@ -13,8 +13,12 @@ struct TitleDetailView: View {
         GeometryReader{ geometry in
             ScrollView{
                 LazyVStack(alignment: .leading){
-                    YoutubePlayer(videoID: "pM7FL8TdB90")
-                        .aspectRatio(1.3, contentMode: .fit)
+//                    YoutubePlayer(videoID: "pM7FL8TdB90")
+//                        .aspectRatio(1.3, contentMode: .fit)
+                    YoutubePlayerView(videoID: "aqz-KE-bpKQ")
+                        .frame(height: 220) // or use aspectRatio if you prefer
+                        .cornerRadius(12)
+                        .padding(.horizontal)
                     Text((title.name ?? title.title) ?? "")
                         .font(.largeTitle)
                         .bold()
