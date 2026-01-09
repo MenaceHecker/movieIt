0931f2bc17af3f5f143d9844533626e66497ca3d
MenaceHecker <thedynamic2508@gmail.com>
2026-01-09 21:30:39 +0530

    Created list for ui

---
+++ movieIt/VerticalListView.swift
@@ -0,0 +1,18 @@
+//
+//  VerticalListView.swift
+//  movieIt
+//
+//  Created by Tushar Mishra on 1/9/26.
+//
+
+import SwiftUI
+
+struct VerticalListView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    VerticalListView()
+}
