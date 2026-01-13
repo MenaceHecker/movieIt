a3360a766caba92227109883022ab074015d55fa
MenaceHecker <thedynamic2508@gmail.com>
2026-01-14 01:08:27 +0530

    Created upcoming view

---
+++ movieIt/UpcomingView.swift
@@ -0,0 +1,18 @@
+//
+//  UpcomingView.swift
+//  movieIt
+//
+//  Created by Tushar Mishra on 1/14/26.
+//
+
+import SwiftUI
+
+struct UpcomingView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    UpcomingView()
+}
