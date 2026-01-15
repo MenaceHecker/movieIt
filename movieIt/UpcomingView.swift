c0af18c3e348651fb5e566aade8e987ff864ef5d
MenaceHecker <thedynamic2508@gmail.com>
2026-01-15 19:39:20 +0530

    Updated geometry reader

--- movieIt/UpcomingView.swift
+++ movieIt/UpcomingView.swift
@@ -10,7 +10,21 @@ import SwiftUI
 struct UpcomingView: View {
     let viewModel = ViewModel()
     var body: some View {
-        VerticalListView(titles: viewModel.upcomingMovies)
+        GeometryReader { geo in
+            switch viewModel.upcomingStatus {
+            case .notStarted:
+                EmptyView()
+            case .fetching:
+                ProgressView()
+                    .frame (width: geo.size.width, height: geo.size.height)
+            case .success:
+                VerticalListView(titles: viewModel.upcomingMovies)
+            
+            case .failed(let underlyingError):
+                Text(underlyingError.localizedDescription)
+            }
+            
+        }
     }
 }
 
