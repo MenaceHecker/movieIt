ebca1b377da5d9fca5679d2fb92654c86bebe7ca
MenaceHecker <thedynamic2508@gmail.com>
2026-01-15 21:12:17 +0530

    LazyVGrid completed

--- movieIt/SearchView.swift
+++ movieIt/SearchView.swift
@@ -8,8 +8,26 @@
 import SwiftUI
 
 struct SearchView: View {
+    var titles = Title.previewTitles
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ScrollView{
+           //LazyVGrid Allows items to stack vertically into columns, Lazy would load items only when they are needed on screen
+            LazyVGrid(columns: [GridItem(), GridItem(),  GridItem()]) {
+                ForEach(titles) { title in
+                    AsyncImage(url: URL(string: title.posterPath ?? "")){
+                        image in
+                        image
+                            .resizable()
+                            .scaledToFit()
+                            .clipShape(.rect(cornerRadius: 10))
+                        
+                    } placeholder: {
+                        ProgressView()
+                    }
+                    .frame(width: 120, height: 200)
+                }
+            }
+        }
     }
 }
 
