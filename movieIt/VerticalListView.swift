a81a3a9fb02edff1c1fa6d2769a9eaff4fe50541
MenaceHecker <thedynamic2508@gmail.com>
2026-01-15 20:59:36 +0530

    Updated nav link inside child view

--- movieIt/VerticalListView.swift
+++ movieIt/VerticalListView.swift
@@ -6,29 +6,35 @@
 //
 
 import SwiftUI
-
+// Added nav stack to parent view , added nav link inside child view
+// nav link allows defining where the user goes and how the link looks using label
 struct VerticalListView: View {
     var titles: [Title]
     
     var body: some View {
         List(titles) { title in
-            AsyncImage(url: (title.posterPath.flatMap(URL.init(string:)))) { image in
-                HStack{
-                    image
-                        .resizable()
-                        .scaledToFit()
-                        .clipShape(.rect(cornerRadius: 10))
-                        .padding(5)
-                    
-                    Text((title.name ?? title.title) ?? "")
-                        .font(.system(size: 14))
-                        .bold()
+            NavigationLink {
+                TitleDetailView(title: title)
+            } label: {
+                AsyncImage(url: (title.posterPath.flatMap(URL.init(string:)))) { image in
+                    HStack{
+                        image
+                            .resizable()
+                            .scaledToFit()
+                            .clipShape(.rect(cornerRadius: 10))
+                            .padding(5)
+                        
+                        Text((title.name ?? title.title) ?? "")
+                            .font(.system(size: 14))
+                            .bold()
+                    }
                 }
+                placeholder: {
+                        ProgressView()
+                    }
+                .frame(height: 150)
             }
-            placeholder: {
-                    ProgressView()
-                }
-            .frame(height: 150)
+            
             }
         }
     }
