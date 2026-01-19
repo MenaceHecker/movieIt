86f57b000f50709a5ac30249d2bd968ad378e34c
MenaceHecker <thedynamic2508@gmail.com>
2026-01-19 16:23:48 -0500

    Declared properties

--- movieIt/SearchViewModel.swift
+++ movieIt/SearchViewModel.swift
@@ -6,3 +6,9 @@
 //
 
 import Foundation
+@Observable
+class SearchViewModel {
+    private(set) var errorMessage: String?
+    private(set) var searchTitles: [Title] = []
+    private let dataFetcher = DataFetcher()
+}
