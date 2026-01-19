9ae452ecfd0c20e1f41bc725ef9c4330093edc8e
MenaceHecker <thedynamic2508@gmail.com>
2026-01-19 16:30:56 -0500

    Created async getSearchTitles func

--- movieIt/SearchViewModel.swift
+++ movieIt/SearchViewModel.swift
@@ -11,4 +11,9 @@ class SearchViewModel {
     private(set) var errorMessage: String?
     private(set) var searchTitles: [Title] = []
     private let dataFetcher = DataFetcher()
+    
+//media will change according to movie or tv search, title will search for the user input, async is used as this function makes network call
+    func getSearchTitles(by media: String, for title: String) async {
+        
+    }
 }
