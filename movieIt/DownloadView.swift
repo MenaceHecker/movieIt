deb6f353da3aa82579d1101c37dce74742d8d63e
MenaceHecker <thedynamic2508@gmail.com>
2026-01-23 06:45:11 -0500

    Displaying status message for empty list

--- movieIt/DownloadView.swift
+++ movieIt/DownloadView.swift
@@ -13,7 +13,14 @@ struct DownloadView: View {
     @Query var savedTitles: [Title]
 // Fetches all data and updates list automatically when db changes
     var body: some View {
-        
+        NavigationStack{
+            if savedTitles.isEmpty{
+                Text("No Downloads Found")
+                    .padding()
+                    .font(.title3)
+                    .bold()
+            }
+        }
     }
 }
 
